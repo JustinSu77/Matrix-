9355d675115086adc4d40690badc8d398f22a5ba
Justin <sujustin77@gmail.com>
2023-05-23 19:01:18 +0100

    Elligance! Nested initializer lists for multi-dimensional arrays

--- MDArray.h
+++ MDArray.h
@@ -4,6 +4,7 @@
 
 #ifndef MDARRAY_H
 #define MDARRAY_H
+#include <iostream>
 #include <initializer_list>
 namespace justin_su
 {
@@ -13,7 +14,7 @@ namespace justin_su
         public:
             MDArray();
             MDArray(int rows, int columns);
-            MDArray(const std::initializer_list<T>& list);
+            MDArray(const std::initializer_list<std::initializer_list<T>>& list);
             ~MDArray();
             void print();
             void fill(T value);
@@ -23,11 +24,82 @@ namespace justin_su
             T valueAt(int row, int column);
             int numberOfRows();
             int numberOfColumns();
+            void operator=(const MDArray<T>& arr);
+            bool operator==(const MDArray<T>& arr);
+
         private:
             int rows;
             int columns;
-            int** array;
+            T** array;
     };
+
+    template<class T>
+    MDArray<T>::MDArray(const std::initializer_list<std::initializer_list<T>> &list): rows(list.size()), columns(list.begin()->size())
+    {
+        int row_elements = 0;
+        int col_elements = 0;
+        array = new T*[rows]{};
+        for (int i = 0; i < rows; i++)
+        {
+            array[i] = new T[columns]{};
+        }
+        for (auto row : list)
+        {
+            for (auto element : row)
+            {
+                array[col_elements][row_elements] = element;
+                row_elements++;
+            }
+            col_elements++;
+            row_elements = 0;
+        }
+    }
+
+    template<class T>
+    MDArray<T>::MDArray(int rows, int columns): rows(rows), columns(columns)
+    {
+        array = new T*[rows]{};
+        for (int i = 0; i < rows; i++)
+        {
+            array[i] = new T[columns]{};
+        }
+
+    }
+
+    template<class T>
+    MDArray<T>::~MDArray()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            delete[] array[i];
+        }
+        delete[] array;
+    }
+
+    template<class T>
+    void MDArray<T>::print()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                std::cout << array[i][j] << " ";
+            }
+            std::cout << std::endl;
+
+        }
+    }
+
+    template<class T>
+    MDArray<T>::MDArray(): rows(3), columns(3)
+    {
+        array = new T*[rows]{};
+        for (int i = 0; i < rows; i++)
+        {
+            array[i] = new T[columns]{};
+        }
+    }
+
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "MDArray.h"
+using namespace justin_su;
+int main()
+{
+    MDArray<int>arr = {{1,2,3},{4,5,6},{7,8}};
+    arr.print();
     return 0;
 }
